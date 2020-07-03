1f283b9cc7badcd756535111e4f5bf850585f2ca
Vaishak <vaishakhvkv@gmail.com>
2020-07-04 01:11:46 +0530

    Updated Database to env

--- db/index.js
+++ db/index.js
@@ -2,7 +2,7 @@ const sqlite3 = require("sqlite3").verbose();
 const path = require("path");
 
 let db = new sqlite3.Database(
-	path.join(__dirname, "database/survey.db"),
+	path.join(__dirname, process.env.DATABASE),
 	(err) => {
 		if (err) {
 			console.log("Error connecting to database", err);
